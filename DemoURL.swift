3b9c8f878cc781f3526b0fb61a058233cc5059b5
dimitar <dimityr.danailov@gmail.com>
2015-09-03 13:46:35 +0300

    Add implementation of Cassini project.

---
+++ DemoURL.swift
@@ -0,0 +1,18 @@
+//
+//  DemoURL.swift
+//  Cassini
+//
+//  Created by dimitar on 9/3/15.
+//  Copyright (c) 2015 dimityr.danailov. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+    static let Stanford = NSURL(string: "http://comm.stanford.edu/wp-content/uploads/2013/01/stanford-campus.png")
+    struct NASA {
+        static let Cassini = NSURL(string: "http://www.nasa.gov/images/content/433394main_pia12843-full.jpg")
+        static let Earth = NSURL(string: "http://www.nasa.gov/sites/default/files/images/115334main_image_feature_329_ys_full.jpg")
+        static let Saturn = NSURL(string: "http://large.stanford.edu/courses/2007/ph210/vo2/images/f1big.jpg")
+    }
+}
